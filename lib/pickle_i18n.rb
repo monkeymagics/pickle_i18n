c85d817dc6ec2637ad4a1e484db105a1d3d887a2
Takahisa Tokunaga <tokunaga.jp@gmail.com>
2011-05-20 11:47:41 +0900

    config/localeで設定される翻訳に:activemodelだけでなく:activerecordが設定された場合にそれを読み込めるようにしたはずです

--- lib/pickle_i18n.rb
+++ lib/pickle_i18n.rb
@@ -16,17 +16,25 @@ module PickleI18n
       Pickle::Parser.send(:include, PickleI18n::Parser)
       Pickle::Session.send(:include, PickleI18n::Session)
 
-
       # pickle_config.factories の中身はこんな感じ
       #   {"product"=>#<Pickle::Adapter::FactoryGirl:0x00000102b0b618 @klass=Product(id: integer, name: string, price: decimal, created_at: datetime, updated_at: datetime), @name="product">}
       # モデルの日本語名についてもfactoryを設定します
       # pickle_config.factories['商品'] = pickle_config.factories['product']
-      I18n.t(:activemodel, :locale => locale).tap do |translations|
-        model_translations.update(translations[:models].stringify_keys.invert)
-        translations[:attributes].each do |model_name, attrs|
-          model_attribute_translations[model_name.to_s] = attrs.stringify_keys.invert
+
+      [:activemodel, :activerecord].each do |scope|
+        begin
+          models_hash = I18n.config.backend.translate(locale, :models, :scope => scope)
+          model_translations.update(models_hash.stringify_keys.invert)
+
+          model_to_attr_hash = I18n.config.backend.translate(locale, :attributes, :scope => scope)
+          model_to_attr_hash.each do |model_name, attr_hash|
+            model_attribute_translations[model_name.to_s] = attr_hash.stringify_keys.invert
+          end
+        rescue I18n::MissingTranslationData
+          # 翻訳が見つからない場合はスルーします
         end
       end
+
       model_translations.each do |key, value|
         pickle_config.factories[key] = pickle_config.factories[value]
       end
